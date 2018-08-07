a4cd34ff9fa05589bb03bbf5f3cbcf05ef7aa85c
Taylor Berg <me@tman0.com>
2018-08-07 12:29:24 -0400

    Various fixes
    Signed-off-by: Taylor Berg <me@tman0.com>

--- cta_clock/providers/clock.py
+++ cta_clock/providers/clock.py
@@ -5,18 +5,18 @@ class ClockProvider(MessageProvider):
     def __init__(self):
         super().__init__()
         self.date_format = '%a %b %-d, %Y'
-        self.time_format_tick = '%-H:%M %p'
-        self.time_format_tock = '%-H %M %p'
+        self.time_format_tick = '%-I:%M %p'
+        self.time_format_tock = '%-I %M %p'
         self.messages = [
             Message(0, self.get_date),
             Message(1, self.get_time)
         ]
 
     def get_date(self):
-        return datetime.utcnow().strftime(self.date_format)
+        return datetime.now().strftime(self.date_format)
 
     def get_time(self):
-        now = datetime.utcnow()
+        now = datetime.now()
         return now.strftime(self.time_format_tick if now.second % 2 == 0 else self.time_format_tock)
 
     def update(self):
